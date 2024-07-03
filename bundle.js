870d1aa29334ba3eeb8448232e0601099e6df54b
Jennifer Aube <62511019+jaube05@users.noreply.github.com>
2024-07-03 10:58:47 -0400

    updating date on homepage for blog with js

--- bundle.js
+++ bundle.js
@@ -17,16 +17,11 @@
       });
       
     
-        // var blogDate = document.getElementById("blog-date").innerHTML;
-        // var elms = document.querySelectorAll("[id='blog-date']").innerHTML;
-        // for(var i = 0; i < elms.length; i++) {
-        //     console.log(elms[i]);
-        //     var oldDate = Date.parse(elms[i]);
-        //     var date = new Date(oldDate).toLocaleDateString("en-US");
-        //     // console.log(elms[i]);
-        //     elms[i].innerHTML = date;
-        // }
-            
+        var blogDate = document.getElementById("blog-date").innerHTML;
+        var oldDate = Date.parse(blogDate);
+        var date = new Date(oldDate).toLocaleDateString("en-US");
+        document.getElementById("blog-date").innerHTML = date;
+    
 
     function mobileFunction() {
         var x = document.getElementById("myLinks");
